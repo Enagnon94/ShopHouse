603121c8a23e4913b7f1f96af4c847866cce85e4
Enagnon94 <ah.farell@gmail.com>
2020-10-16 22:03:00 +0200

    modif

--- public/accueil.php
+++ public/accueil.php
@@ -7,10 +7,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 
-<?php 
-    echo ("COU");
-?>
-
 <body>
 
     <header id="header">
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,32 @@
+
+var onglet = document.querySelector("meta").getAttribute("name");
+
+window.onload = function () {
+
+	this.buildPage();						/* Construction header et footer*/
+	this.menuItemColor(onglet);				/* Coloration item menu selectionné*/
+}
+
+
+window.onscroll = function () { 
+
+	this.mySticky();						/* Ajout sitcky ou non*/
+}   
+
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
 /*-------------------------------------------------------Builder de page pour afficher le header et footer automatiquement
 */
 function buildPage() {
@@ -14,12 +43,42 @@ function buildPage() {
 
 /*------------------------------------------------------Gestion menu
 */
-var onglet = document.querySelector("meta").getAttribute("name");
-window.onload = function () {
 
-	this.buildPage();
 
-	switch (this.onglet) {
+function scrollToTop(scrollDuration) {
+	var scrollStep = -window.scrollY / (scrollDuration / 15),
+		scrollInterval = setInterval(function () {
+			if (window.scrollY != 0) {
+				window.scrollBy(0, scrollStep);
+			}
+			else clearInterval(scrollInterval);
+		}, 15);
+	document.body.scrollTop = 0;
+}
+  
+/*------------------------------------------------------Gestion menu
+*/
+
+
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function mySticky() {
+	// Get the header
+	var header = document.getElementById('myTopnav');
+	// Get the offset position of the navbar
+	var sticky = myTopnav.offsetTop;
+	if (window.pageYOffset > sticky) {
+		header.classList.remove("sticky");
+		header.classList.add("sticky");
+
+	} else {header.classList.add("sticky");
+		header.classList.remove("sticky");
+	}
+}
+
+function menuItemColor(onglet){
+
+	switch (onglet) {
 		case "Accueil":
 			document.getElementById("1a").classList.add("active");
 			break;
@@ -38,15 +97,4 @@ window.onload = function () {
 			break;
 	}
 
-}
-
-function scrollToTop(scrollDuration) {
-	var scrollStep = -window.scrollY / (scrollDuration / 15),
-		scrollInterval = setInterval(function () {
-			if (window.scrollY != 0) {
-				window.scrollBy(0, scrollStep);
-			}
-			else clearInterval(scrollInterval);
-		}, 15);
-	document.body.scrollTop = 0;
-}
+};
