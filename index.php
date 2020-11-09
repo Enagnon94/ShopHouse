43f9b630314eadae64f6788a0324bedbf31c7d87
AykLoveTTC <aykel.cheniour@cpe.fr>
2020-11-09 15:59:21 +0100

    test2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ try {
     $bdd = new PDO("mysql:host=localhost;dbname=ShopHouseDB", "Enagnon",  "bdd");
     $bdd->query("SET NAMES UTF8");
 } catch (Exception $e) {
-    echo "Problème de connexion à la base de donnée !";
+    echo "Problème de connexion à la base de donnée!";
     die();
 }
 echo("Ça marche !!!");
